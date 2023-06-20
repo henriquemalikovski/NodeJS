5683d8d1d6416f9d413d374d64dfc48cbd4d40c6
Henrique Malikovski <hmalikovski@gmail.com>
2023-06-20 12:29:57 -0300

    Criando o servidor

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express()
+const router = express.Router()
+
+const route = router.get('/', (req, res, next) => {
+  res.status(200).send({
+    title: "Node Store API",
+    version: "0.0.1"
+  })
+})
+
+const create = router.post('/', (req, res, next) => {
+  res.status(200).send()
+})
+
+app.use('/', route)
+
+module.exports = app
